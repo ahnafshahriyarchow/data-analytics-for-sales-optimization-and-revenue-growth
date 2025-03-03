70de82dc3747b71a97466f3d617156b2951765cb
Ahnaf Shahriyar Chowdhury <ahnafshahriyarchow@gmail.com>
2025-03-03 02:45:39 -0500

    Add files via upload

---
+++ SparkSQL_commands/data_cleaning.sql
@@ -0,0 +1,13 @@
+﻿spark.sql("""
+CREATE OR REPLACE TEMP VIEW amazon_new AS
+SELECT
+product_id,
+product_name,
+category,
+CAST(REGEXP_REPLACE(discounted_price, '[₹,]', '') AS FLOAT) AS discounted_price,
+CAST(REGEXP_REPLACE(actual_price, '[₹,]', '') AS FLOAT) AS actual_price,
+CAST(REGEXP_REPLACE(discount_percentage, '[%]', '') AS FLOAT) AS discount_percentage,
+CAST(REGEXP_REPLACE(rating_count, '[,]', '') AS FLOAT) AS rating_count,
+rating
+FROM Amazon
+""").show()
