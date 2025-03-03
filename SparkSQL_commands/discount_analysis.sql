90b3ac39471c8ca4395b88e1049df1e8ac469ad2
Ahnaf Shahriyar Chowdhury <ahnafshahriyarchow@gmail.com>
2025-03-03 17:27:46 -0500

    Add files via upload

---
+++ SparkSQL_commands/discount_analysis.sql
@@ -0,0 +1,20 @@
+﻿spark.sql("""
+CREATE OR REPLACE TEMP VIEW discount_analysis_new AS
+SELECT
+category,
+CASE
+WHEN discount_percentage <= 0.1 THEN '0-10%'
+WHEN discount_percentage > 0.1 AND discount_percentage <= 0.2 THEN '10-20%'
+WHEN discount_percentage > 0.2 AND discount_percentage <= 0.3 THEN '20-30%'
+WHEN discount_percentage > 0.3 AND discount_percentage <= 0.4 THEN '30-40%'
+WHEN discount_percentage > 0.4 AND discount_percentage <= 0.5 THEN '40-50%'
+ELSE '50%+'
+END AS DiscountRange,
+AVG(NormalizedSalesVolume) AS AvgNormalizedSalesVolume,
+AVG(NormalizedInventoryPerformance) AS AvgNormalizedInventoryPerformance,
+AVG(rating) AS AvgRating,
+AVG(discount_percentage) AS AvgDiscountPercentage,
+AVG(NormalizedRevenue) AS AvgNormalizedRevenue
+FROM amazon_sales
+GROUP BY category, DiscountRange
+""").show()
