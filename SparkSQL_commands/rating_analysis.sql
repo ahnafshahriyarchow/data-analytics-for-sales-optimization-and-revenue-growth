6f8880557dd050d509516003611a942ee0df50e5
Ahnaf Shahriyar Chowdhury <ahnafshahriyarchow@gmail.com>
2025-03-03 03:06:23 -0500

    Add files via upload

---
+++ SparkSQL_commands/rating_analysis.sql
@@ -0,0 +1,17 @@
+﻿spark.sql("""
+CREATE OR REPLACE TEMP VIEW rating_analysis AS
+SELECT
+category,
+CASE
+WHEN rating <= 2 THEN 'Low Ratings (1.0-2.0)'
+WHEN rating > 2 AND rating <= 4 THEN 'Medium Ratings (2.1-4.0)'
+ELSE 'High Ratings (4.1-5.0)'
+END AS RatingRange,
+COUNT(*) AS ProductCount,
+AVG(NormalizedSalesVolume) AS AvgNormalizedSalesVolume,
+AVG(NormalizedRevenue) AS AvgNormalizedRevenue,
+AVG(NormalizedInventoryPerformance) AS AvgNormalizedInventoryPerformance,
+AVG(discount_percentage) AS AvgDiscountPercentage
+FROM amazon_sales
+GROUP BY category, RatingRange
+""").show()
